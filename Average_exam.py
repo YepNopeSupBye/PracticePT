68f6cf6184737fc54c808268b32fbaa8011236e2
YepNopeSupBye <rmurphy.25@acsamman.edu.jo>
2023-04-03 10:15:55 +0300

    Finshed Code

--- Average_exam.py
+++ Average_exam.py
@@ -4,8 +4,19 @@ def get_scores():
     # Get input from the user for three exam scores
     exam_scores = []
     print("Scores range from 1 - 100")
-    for i in range(3):
-        score = float(input("Enter score for exam " + str(i+1) + ": "))
+    while True:
+        try:
+            User_Input = int(input("How Many Exams Have You Had? "))
+            break
+        except ValueError:
+            print("Enter int")
+    for i in range(User_Input):
+        while True:
+            try:
+                score = float(input("Enter score for exam " + str(i+1) + ": "))
+                break
+            except ValueError:
+                print("Enter float")
         exam_scores.append(score)
     return exam_scores
 
@@ -15,7 +26,7 @@ def calculate_average(scores_list):
     for j in scores_list:
         sum += j
     average = sum / len(scores_list)
-    return float(average)
+    return float(round(average, 2))
 
 # Call the get_scores function to get the exam scores
 scores_list = get_scores()
